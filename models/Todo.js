cbf5d98aa847d76f04031e362806be4fce288edb
Ankita Dubey <ankita03022003@gmail.com>
2026-08-18 17:48:15 +0530

    backend things

---
+++ models/Todo.js
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose';
+
+const TodoSchema = new mongoose.Schema({
+  text: { type: String, required: true },
+  completed: { type: Boolean, default: false },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
+}, { timestamps: true });
+
+export default mongoose.model('Todo', TodoSchema);
